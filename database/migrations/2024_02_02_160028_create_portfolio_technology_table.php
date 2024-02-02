79fd8a30b8112f8913347bde828af6e1f45b1619
Marco <marcoregina92@gmail.com>
2024-02-02 17:18:11 +0100

    create new table

---
+++ database/migrations/2024_02_02_160028_create_portfolio_technology_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('portfolio_technology', function (Blueprint $table) {
+            // $table->id();
+            // $table->timestamps();
+
+            $table->foreignId('portfolio_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('technology_id')->constrained()->cascadeOnDelete();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('portfolio_technology');
+    }
+};
